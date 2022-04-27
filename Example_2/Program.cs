3ac93647b49522d1024e80bb7ee5b3c963e3177f
VladimirTialin <103684564+VladimirTialin@users.noreply.github.com>
2022-04-27 23:01:28 +0300

    Решили вторую задачу

---
+++ Example_2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 2: Напишите программу, которая принимает на
+вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+int[] numbers = new int [3];
+Console.WriteLine("Введите 3 числа: ");
+int MaxNumber = numbers[0];
+for (byte i = 0; i < numbers.Length; i++)
+{
+    int number = Convert.ToInt32(Console.ReadLine());
+    numbers[i] = number;
+}
+for (byte i = 0; i < numbers.Length; i++)
+{
+    if (MaxNumber < numbers[i])
+    {
+        MaxNumber = numbers[i];
+    }
+}
+Console.WriteLine("Max = "+MaxNumber);
