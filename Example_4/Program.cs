ab213d9ed25c0069c371c2d7bac1e96a9cf3c82b
VladimirTialin <103684564+VladimirTialin@users.noreply.github.com>
2022-04-27 23:31:48 +0300

    Решили четвертую задачу

---
+++ Example_4/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 4: Напишите программу, которая на вход
+принимает число (N), а на выходе показывает все чётные
+числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Найдены следующие чётные числа: ");
+if (number == 0)
+{
+    Console.Write(number);
+}
+    for (int i = 1; i <= number; i++)
+    {
+
+        if (i % 2 == 0)
+        {
+            Console.Write(i);
+            if (i != number & i!= number-1)
+            {
+                Console.Write(", ");
+            }
+        }
+
+    }
